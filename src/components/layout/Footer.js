4eeaaaccb395b65dbdc48a31e272f8f45b896e59
Ringo007 <ringo@Prajwals-Air.domain.name>
2019-11-24 23:09:27 +0545

    First Commit

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Footer() {
+    return (
+    <div>
+        <div className="row">
+            <div className="col-md-12">
+                <div className="footer p-3 mt-4 text-center bg-dark text-light">
+                Developed By:
+                <span className="text-warning font-weight-normal">
+                Prajwal Rawal
+                </span>
+                , Using <i className="fab fa-react" /> React JS , Redux JS &amp;
+                data API
+                    <a
+                    href="http://www.omdbapi.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    _OMDB
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Footer
